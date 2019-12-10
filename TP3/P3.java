4efd58c53101d85211edd1125f1024af7a53484a
nayaras <nayarathaiza@gmail.com>
2019-12-09 23:21:24 -0300

    finaliza p3

--- TP3/P3.java
+++ TP3/P3.java
@@ -16,6 +16,8 @@ public class P3{
 	ArrayList<Integer> posicoes = new ArrayList<Integer>();
 	//ArrayList<ArrayList<Integer>> populacao = new ArrayList<ArrayList<Integer>>();
 	ArrayList<Integer> solucoes = new ArrayList<Integer>();
+	ArrayList<Integer> pai1 = new ArrayList<Integer>();
+	ArrayList<Integer> pai2 = new ArrayList<Integer>();
 
 	public int solucao_inicial(ArrayList<Integer> entrada, ArrayList<Integer> posicoes){
 	
@@ -124,6 +126,13 @@ public class P3{
 		}
 		return prob;
 	}	
+	public void copia_pais(int ind1, int ind2, int [][] pop){
+		for(int i = 0; i < posicoes.size(); ++i){
+			pai1.add(pop[ind1][i]);
+			pai2.add(pop[ind2][i]);
+		}
+	}
+	
 	
 	public static void main(String[] args) throws ParseException {
 	
@@ -138,6 +147,8 @@ public class P3{
 		ArrayList<Double> ci = new ArrayList<Double>();
 		
 		int sol_inicial = 0;
+		int num_geracoes = 0;
+		int max_geracoes = 2;
 		
 		//leitura do arquivo de entrada	
 		if (args.length == 1) {
@@ -145,7 +156,7 @@ public class P3{
 				FileReader arq = new FileReader(args[0]);
 				BufferedReader lerArq = new BufferedReader(arq);
 				String linha = lerArq.readLine(); // lê a primeira linha (tamanho da entrada)
-        		System.out.println("\nEntrada: ");
+
 				while (linha != null) {
 					//System.out.printf("%s\n", linha);
 					entrada.add(Integer.parseInt(linha));
@@ -186,25 +197,147 @@ public class P3{
 			int sol_random = inst.executa(entrada_random);
 			inst.solucoes.add(sol_random);
 		}
-		rank_final = inst.ranking(inst.solucoes);
 		
-		pi = inst.probabilidade(2.0, (int)inst.solucoes.size(), rank_final);
+		/*
+		for(int i = 0; i < 40; ++i){
+			for(int j = 0; j < size; ++j){
+				System.out.print(populacao[i][j] + " ");
+			}
+			System.out.print("\nSol = "+ inst.solucoes.get(i)+"\n");
+		}*/
 		
-		//probabilidaade comulativa
-		ci.add(pi.get(0));
-		for(int i = 1; i < pi.size(); ++i){
-			ci.add(pi.get(i)+ci.get(i-1));
-		}
-		//random
-		Random r = new Random(); 
-        double num_sorteio = r.nextDouble();
+		
+		int ultimos = 32;//ultimos 20%
+		while(num_geracoes < max_geracoes){
+			ArrayList<Integer> f1 = new ArrayList<Integer>();//filhos
+			ArrayList<Integer> f2 = new ArrayList<Integer>();
+			ArrayList<Integer> entrada_filho1 = new ArrayList<Integer>();//filhos
+			ArrayList<Integer> entrada_filho2 = new ArrayList<Integer>();
+			
+			rank_final = inst.ranking(inst.solucoes);
+			
+			pi = inst.probabilidade(2.0, (int)inst.solucoes.size(), rank_final);
+			
+			//probabilidaade comulativa
+			ci.add(pi.get(0));
+			for(int i = 1; i < pi.size(); ++i){
+				ci.add(pi.get(i)+ci.get(i-1));
+			}
+			int index = 8;//20% nao muda
+			while(index < 24){//60% da populacao
+				//random
+				Random r = new Random(); 
+				
+		    	//System.out.println(num_sorteio + " "+num_sorteio2);
+		    
+				int ind1 = -1;
+				int ind2 = -1;
+				while(ind1 == -1 || ind2 == -1){
+					double num_sorteio = r.nextDouble();
+					double num_sorteio2 = r.nextDouble();
 
-        if(num_sorteio < 1/entrada.size()){//faz swap
-        	System.out.println("Num sorteado: "+ num_sorteio);
-        }
-        
-		
-		
+					for(int i = 1; i < ci.size(); ++i){
+						if(ci.get(i-1) <= num_sorteio && num_sorteio < ci.get(i) && ind1 == -1){
+							ind1 = i;
+						}
+						if(ci.get(i-1) <= num_sorteio2 && num_sorteio2 < ci.get(i) && ind2 == -1){
+							ind2 = i;
+						}
+						if(ind1 != -1 && ind2 != -1)
+							break;				
+					}
+				}
+				
+				
+				
+				//System.out.println(ind1 + " "+ind2);
+				inst.copia_pais(ind1, ind2, populacao);
+				
+				for(int i = 0; i < inst.solucoes.size(); ++i){
+					double num_sorteio3 = r.nextDouble();
+					if(num_sorteio3 < (1/inst.pai1.size())){
+						Collections.swap(inst.pai1, i, i+1);
+						Collections.swap(inst.pai2, i, i+1);
+					}
+				}
+				//System.out.println(inst.pai1);
+				//System.out.println(inst.pai2);
+								System.out.println();
+				for(int i = 0; i < inst.pai1.size()/2; ++i){
+					if(f2.size() != size && f1.size() != size){
+						f1.add(inst.pai1.get(i));
+						f2.add(inst.pai2.get(i));
+					}
+				}
+				for(int i = 0; i < size; ++i){
+					if(f1.indexOf(inst.pai2.get(i)) == -1 && f1.size() != size){
+						f1.add(inst.pai2.get(i));
+					}
+					if(f2.indexOf(inst.pai1.get(i)) == -1 && f2.size() != size){
+						f2.add(inst.pai1.get(i));
+					}
+				}
+				//System.out.println(f1);
+				//System.out.println(f2);
+				
+				entrada_filho1 = inst.reconstroi_entrada(f1, entrada_inicial);
+				entrada_filho2 = inst.reconstroi_entrada(f2, entrada_inicial);
+				
+				int sol1 = inst.executa(entrada_filho1);
+				int sol2 = inst.executa(entrada_filho2);
+				//System.out.println("sol1: "+sol1+" sol2: "+sol2);
+				
+					if(sol1 < inst.solucoes.get(index)){
+						for(int i = 0; i < size; ++i){
+							//System.out.println("index: "+index);
+							populacao[index][i] = f1.get(i);
+							inst.solucoes.set(index, sol1);
+							index++;
+							break;
+						}
+					}
+					else{
+						for(int i = 0; i < size; ++i){
+							if(ultimos < 40){
+								populacao[ultimos][i] = f1.get(i);
+								inst.solucoes.set(ultimos, sol1);
+								ultimos++;
+							}
+						}
+					}
+					if(sol2 < inst.solucoes.get(index)){
+						for(int i = 0; i < size; ++i){
+							//System.out.println("index: "+index);
+							populacao[index][i] = f2.get(i);
+							inst.solucoes.set(index, sol2);
+							index++;
+							break;
+						}
+					}
+					else{
+						for(int i = 0; i < size; ++i){
+							if(ultimos < 40){
+								populacao[ultimos][i] = f2.get(i);
+								inst.solucoes.set(ultimos, sol2);
+								ultimos++;
+							}
+						}
+					}
+					
+				index++;
+				inst.pai1.clear();
+				inst.pai2.clear();
+				f1.clear();
+				f2.clear();
+				
+
+			}
+			
+			
+       	int sol_min = Collections.min(inst.solucoes);
+       	System.out.println("Solucao minima da geracao = "+ sol_min);
+		num_geracoes++;
+		}
 		
 	}
 }
@@ -214,6 +347,13 @@ for(int i = 0; i < 40; ++i){
 				System.out.print(populacao[i][j] + " ");
 			}
 			System.out.print("\nSol = "+ inst.solucoes.get(i)+"\n");
+		}
+		
+		for(int i = 0; i < 40; ++i){
+			for(int j = 0; j < size; ++j){
+				System.out.print(populacao[i][j] + " ");
+			}
+			System.out.println();
 		}
 		
 		
